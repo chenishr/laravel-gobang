2af44dce60f473cf2b7b93581d441c6b42ab56c3
chenishr <chenishr@gmail.com>
2017-08-07 13:10:31 +0800

    后台逻辑

--- app/Http/Controllers/GobangController.php
+++ app/Http/Controllers/GobangController.php
@@ -9,13 +9,47 @@ use Illuminate\Support\Facades\Redis;
 class GobangController extends Controller {
 	const	WAITTING	= 'GOBANG_WAITING';
     //
-	public function index(){
-		//$users	= DB::select('select * from user');
-		//var_dump($users);
+	public function index(Request $request){
+		if(!$this->check_user($request)){
+			echo 'login please!';
+			exit;
+		}
 
 		$this->create_chess(1);
 
 		return view('gobang.index');
+	}
+
+	// 检查用户是否存在会话
+	protected function check_user($request){
+		if($request->has('user')){
+			session('userId',$request->has('user'));
+			session('gobangId',$request->has('gobang'));
+		}
+
+		$userId		= session('userId');
+		$gobangId	= session('gobangId');
+
+		$chess	= $this->get_gobang_chess($gobangId);
+	}
+
+	protected function set_gobang_user($gobangId,$userId,$client){
+		$gobang	= Redis::hset('GOBANG:'.$gobangId,'USER:'.$userId,$client);
+	}
+
+
+	protected function set_gobang_chess($gobangId,$chess){
+		$gobang	= Redis::hset('GOBANG:'.$gobangId,'CHESS',$chess);
+	}
+
+
+	protected function get_gobang_user($gobangId,$userId){
+		$gobang	= Redis::hget('GOBANG:'.$gobangId,'USER:'.$userId);
+	}
+
+
+	protected function get_gobang_chess($gobangId){
+		$gobang	= Redis::hget('GOBANG:'.$gobangId,'CHESS');
 	}
 
 	protected function create_chess(int $userId){
